3e05522c01a9e3bcf65812b6b82428ffaa8d4854
William Borda <wborda@emeal.nttdata.com>
2023-07-11 15:51:26 -0500

    update Code Smell

--- Client_Microservice/src/main/java/com/nttdata/com/client_microservice/repository/CreditProductRepository.java
+++ Client_Microservice/src/main/java/com/nttdata/com/client_microservice/repository/CreditProductRepository.java
@@ -1,6 +1,5 @@
 package com.nttdata.com.client_microservice.repository;
 
-import com.nttdata.com.client_microservice.model.Account;
 import com.nttdata.com.client_microservice.model.CreditProduct;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
